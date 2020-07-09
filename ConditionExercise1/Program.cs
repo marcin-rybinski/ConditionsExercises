78a763ea8a493c9c3e1dad54ea26d7c025b12160
Marcin Rybiński <rybinski.marcin@gmail.com>
2020-07-09 19:38:42 +0200

    exercise1 project added

---
+++ ConditionExercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConditionExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj trzy liczby, żeby sprawdzić, która z nich jest największa: ");
+            double firstNumber = Double.Parse(Console.ReadLine());
+            double secondNumber = Double.Parse(Console.ReadLine());
+            double thirdNumber = Double.Parse(Console.ReadLine());
+            var highest = (firstNumber > secondNumber) ? firstNumber : secondNumber;
+            highest = (highest > thirdNumber) ? highest : thirdNumber;
+            Console.WriteLine($"Najwyższa liczba to: {highest}");
+        }
+    }
+}
---
+++ ConditionExercise2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConditionExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj liczbę, a ja sprawdzę czy jest podzielna zarówno przez 5 jak i przez 11: "); 
+            var 
+            if ()
+        }
+    }
+}
