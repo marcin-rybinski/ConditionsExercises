1e603b04da4c4789452da101a2fec59636eb0a2f
Marcin Rybiński <rybinski.marcin@gmail.com>
2020-07-10 14:37:06 +0200

    Exercise3 done

---
+++ ConditionExercise3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConditionExercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Naciśnij dowolny klawisz: ");
+            var input = Console.ReadKey();
+            var inputChar = Convert.ToChar(input.KeyChar);
+            Console.WriteLine();
+            if (inputChar >= 'a' && inputChar <= 'z' || inputChar >= 'A' && inputChar <= 'Z') Console.WriteLine("Wcisnąłeś literę.");
+            else Console.WriteLine("Wcisnąłeś inny klawisz.");
+
+            
+            
+            //another way to do this
+
+            //if (Char.IsLetter(Convert.ToChar(input))) Console.WriteLine("Wcisnąłeś literę.");
+            //else Console.WriteLine("Wcisnąłeś inny klawisz.");
+        }
+    }
+}
